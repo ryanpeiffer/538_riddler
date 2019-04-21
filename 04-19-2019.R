c955abc6b8b65c8f3c52b83341abb189b7930b80
ryanpeiffer <ryanpeiff@gmail.com>
2019-04-21 16:11:59 -0500

    added additional comments

--- 04-19-2019.R
+++ 04-19-2019.R
@@ -6,14 +6,14 @@
 # points are on only one side of the newly halved circle?
 
 #after playing around, it looks like once n>20 the probability is near 0
-n_vals <- rep(1:20)
+n_vals <- rep(1:20) #vector of different values of n to test
 percents <- rep(0,length(n_vals))
-n_trials <- 1e5
+n_trials <- 1e5 #number of sims per value of n
 
 for (i in 1:length(n_vals)) {
     n <- n_vals[i]
-    rand_points <- runif(n*n_trials) * 360
-    wins <- 0
+    rand_points <- runif(n*n_trials) * 360 #generate a vector of random points on circle
+    wins <- 0 #let a "win" be when the points can be captured by one diameter line
     for (j in 1:n_trials) {
         max_point <- max(rand_points[((j-1)*n+1):(j*n)])
         min_point <- min(rand_points[((j-1)*n+1):(j*n)])
@@ -26,3 +26,8 @@ plot(n_vals,percents)
 
 #don't love that I have this double for loop... but this solution is much
 #better than my first pass at it! Should try to continue vectorizing this.
+
+#Now we have a vector of values for N and percent change of it working, how to determine relationship...
+#Shape of our plot suggests percent decreases exponentially with n...
+
+
