771286ae618f049c6d2f4a70378f89e9583e693d
ryanpeiffer <ryanpeiff@gmail.com>
2020-12-05 23:29:25 -0500

    update to include a more nuanced approach

--- 04-26-2019.R
+++ 04-26-2019.R
@@ -6,27 +6,39 @@
 # Out of 7.5 billion people on Earth, how many can we expect would survive?
 #     
 # If there were N Thanoses, what would the survival fraction be?
-options(digits=5, scipen=100)
-library(ggplot2)
 
 
 population <- 7.5e9
 n_thanos <- 63
-prob_survival = 0.5
+prob_survival <- 0.5
 
-survivors <- population * (prob_survival ^ n_thanos)
-survivors
 
-#63 thanoses (thanii?) is enough to make it basically impossible to survive.
+#simplest answer: Thanos snaps only affect humans
+remaining_pop1 <- floor(population * prob_survival^n_thanos)
+#answer = 0
 
-thanos_vec <- c(1:30)
-survivor_vec <- population * (prob_survival ^ thanos_vec)
-prob_survival_vec <- survivor_vec / population
 
-gg_df <- data.frame(thanos_vec,survivor_vec,prob_survival_vec)
-ggplot(gg_df, aes(thanos_vec,survivor_vec)) +
-    geom_point() +
-    theme_minimal() +
-    ylab("number of survivors") +
-    xlab("number of thanii")
+#slightly more nuanced answer: each Thanos snap destroys exactly 50% of all humans and exactly 50% of all thanii
+snap_yo_finger <- function(population, n_thanos) {
+    pop <- population
+    thanos_count <- n_thanos
+    while(thanos_count > 0) {
+        thanos_count <- thanos_count - 1 #reduce thanos count by 1 since this one has now snapped
+        pop <- floor(pop * prob_survival)
+        thanos_count <- floor(thanos_count * prob_survival) #half of the unsnapped thanii also get obliterated
+    }
+    return(pop)
+}
+
+remaining_pop2 <- snap_yo_finger(population, n_thanos)
+#answer = 117,187,500 ... a much nicer situation than the first!
+
+
+#ultimate nuance: assume the universe of all beings is humans+thanii. Randomly simulate whether each living Thanos is
+#destroyed by a given snap. Theoretically this means the number of humans shouldn't get cut exactly in half, but I'm
+#going to keep it simple. You could also go down a rabbit hole of saying the # beings in the universe is likely infinite,
+#so you should really simulate whether each human falls into the destroyed half.... but that's too much for me.
+
+
+
 
